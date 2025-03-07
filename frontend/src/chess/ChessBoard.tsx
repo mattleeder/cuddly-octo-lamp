83cacceeef00c5dd593224c71274fffa4afbe26c
Matt <mattleeder@posteo.co.uk>
2025-03-07 02:51:21 +0000

    Made board resizeable

--- frontend/src/chess/ChessBoard.tsx
+++ frontend/src/chess/ChessBoard.tsx
@@ -55,8 +55,10 @@ async function clickHandler(
     throw new Error("Bounding rect for board is not defined")
   }
 
-  const boardXPosition = Math.floor((event.clientX - rect.current.left) / (rect.current.width / 8))
-  const boardYPosition = Math.floor((event.clientY - rect.current.top) / (rect.current.height / 8))
+  const squareWidth = rect.current.width / 8
+
+  const boardXPosition = Math.floor((event.clientX - rect.current.left) / squareWidth)
+  const boardYPosition = Math.floor((event.clientY - rect.current.top) / squareWidth)
   let position = boardYPosition * 8 + boardXPosition
 
   console.log(`Clicked on ${event.clientX}x, ${event.clientY}y which is position ${position}`)
@@ -240,7 +242,7 @@ function getRowAndColFromBoardIndex(idx: number, flip: boolean): [number, number
   return [row, col]
 }
 
-function PiecesComponent({ flip }: { flip: boolean }) {
+function PiecesComponent({ flip, squareWidth }: { flip: boolean, squareWidth: number }) {
   const game = useContext(GameContext)
   if (!game) {
     throw new Error("PiecesComponent must be called from within a GameContext")
@@ -255,33 +257,56 @@ function PiecesComponent({ flip }: { flip: boolean }) {
       
       const [row, col] = getRowAndColFromBoardIndex(idx, flip)
       return (
-        <div key={idx} className={`${colourToString.get(colour)}-${variantToString.get(variant)}`} style={{ transform: `translate(${col * 50}px, ${row * 50}px)` }} />
+        <div 
+          key={idx} 
+          className={`${colourToString.get(colour)}-${variantToString.get(variant)}`} 
+          style={{ 
+            transform: `translate(${col * squareWidth}px, ${row * squareWidth}px)`,
+            width: `${squareWidth}px`,
+            height: `${squareWidth}px`,
+            backgroundSize: `${squareWidth}px`,
+          }} 
+        />
       )
     })
   )
 }
 
-function MovesComponent({ moves, flip }: { moves: number[], flip: boolean }) {
+function MovesComponent({ moves, flip, squareWidth }: { moves: number[], flip: boolean, squareWidth: number }) {
   return (
     (moves).map((move, idx) => {
       const [row, col] = getRowAndColFromBoardIndex(move, flip)
       return (
-        <div key={idx} className='potential-move' style={{ transform: `translate(${col * 50}px, ${row * 50}px)` }} />
+        <div 
+          key={idx} 
+          className='potential-move' 
+          style={{ 
+            transform: `translate(${col * squareWidth}px, ${row * squareWidth}px)`,
+            backgroundSize: `${squareWidth}px`,
+          }} 
+        />
       )
     }))
 }
 
-function CapturesComponent({ captures, flip }: { captures: number[], flip: boolean }) {
+function CapturesComponent({ captures, flip, squareWidth }: { captures: number[], flip: boolean, squareWidth: number }) {
   return (
     captures.map((move, idx) => {
       const [row, col] = getRowAndColFromBoardIndex(move, flip)
       return (
-        <div key={idx} className='potential-capture' style={{ transform: `translate(${col * 50}px, ${row * 50}px)` }} />
+        <div 
+          key={idx} 
+          className='potential-capture' 
+          style={{ 
+            transform: `translate(${col * squareWidth}px, ${row * squareWidth}px)`,
+            backgroundSize: `${squareWidth}px`,
+          }} 
+        />
       )
     }))
 }
 
-function LastMoveComponent({ flip }: { flip: boolean }) {
+function LastMoveComponent({ flip, squareWidth }: { flip: boolean, squareWidth: number }) {
   const game = useContext(GameContext)
   if (!game) {
     throw new Error("LastMoveComponent must be called from within a GameContext")
@@ -293,12 +318,19 @@ function LastMoveComponent({ flip }: { flip: boolean }) {
       }
       const [row, col] = getRowAndColFromBoardIndex(move, flip)
       return (
-        <div key={idx} className='last-move' style={{ transform: `translate(${col * 50}px, ${row * 50}px)` }} />
+        <div 
+          key={idx} 
+          className='last-move' 
+          style={{ 
+            transform: `translate(${col * squareWidth}px, ${row * squareWidth}px)`,
+            backgroundSize: `${squareWidth}px`,
+          }} 
+        />
       )
     }))
 }
 
-function PromotionComponent({ promotionSquare, promotionActive, flip }: { promotionSquare: number, promotionActive: boolean, flip: boolean }) {
+function PromotionComponent({ promotionSquare, promotionActive, flip, squareWidth }: { promotionSquare: number, promotionActive: boolean, flip: boolean, squareWidth: number }) {
   if (!promotionActive) {
     return <></>
   }
@@ -314,15 +346,42 @@ function PromotionComponent({ promotionSquare, promotionActive, flip }: { promot
 
   return (
     <>
-      <div className={`${promotionColour}-queen promotion`} style={{ transform: `translate(${col * 50}px, ${0 * 50}px)` }} />
-      <div className={`${promotionColour}-knight promotion`} style={{ transform: `translate(${col * 50}px, ${1 * 50}px)` }} />
-      <div className={`${promotionColour}-rook promotion`} style={{ transform: `translate(${col * 50}px, ${2 * 50}px)` }} />
-      <div className={`${promotionColour}-bishop promotion`} style={{ transform: `translate(${col * 50}px, ${3 * 50}px)` }} />
+      <div 
+        className={`${promotionColour}-queen promotion`} 
+        style={{ 
+          transform: `translate(${col * squareWidth}px, ${0 * squareWidth}px)`,
+          backgroundSize: `${squareWidth}px`,
+        }} 
+      />
+
+      <div 
+        className={`${promotionColour}-knight promotion`} 
+        style={{ 
+          transform: `translate(${col * squareWidth}px, ${1 * squareWidth}px)`,
+          backgroundSize: `${squareWidth}px`,
+        }} 
+      />
+
+      <div 
+        className={`${promotionColour}-rook promotion`} 
+        style={{ 
+          transform: `translate(${col * squareWidth}px, ${2 * squareWidth}px)`,
+          backgroundSize: `${squareWidth}px`,
+        }} 
+      />
+
+      <div 
+        className={`${promotionColour}-bishop promotion`} 
+        style={{ 
+          transform: `translate(${col * squareWidth}px, ${3 * squareWidth}px)`,
+          backgroundSize: `${squareWidth}px`,
+        }} 
+      />
     </>
   )
 }
 
-function GameOverComponent() {
+function GameOverComponent({ squareWidth }: { squareWidth: number }) {
   const game = useContext(GameContext)
   if (!game) {
     throw new Error("LastMoveComponent must be called from within a GameContext")
@@ -335,7 +394,7 @@ function GameOverComponent() {
   const gameOverStatusCodes = ["Ongoing", "Stalemate", "Checkmate", "Threefold Repetition", "Insufficient Material", "White Flagged", "Black Flagged", "Draw", "White Resigned", "Black Resigned", "Game Aborted", "White Disconnected", "Black Disconnected"]
   const gameOverText = gameOverStatusCodes[game?.matchData.gameOverStatus || 0]
 
-  return <div style={{ transform: `translate(${0}px, ${180}px)`, color: "black" }}>{gameOverText}</div>
+  return <div style={{ transform: `translate(${0}px, ${squareWidth * 4}px)`, color: "black" }}>{gameOverText}</div>
 }
 
 function getRect(top: number, left: number, width: number, height: number): Rect {
@@ -360,6 +419,7 @@ export function ChessBoard() {
   const [promotionNextMove, setPromotionNextMove] = useState(false)
   const [promotionActive, setPromotionActive] = useState(false)
   const [promotionSquare, setPromotionSquare] = useState(0)
+  const [boardWidth, setBoardWidth] = useState(800)
   
   const game = useContext(GameContext)
   if (!game) {
@@ -372,6 +432,7 @@ export function ChessBoard() {
       for (const entry of entries) {
         const boundingRect = entry.target.getBoundingClientRect()
         rect.current = getRect(boundingRect.top, boundingRect.left, boundingRect.width, boundingRect.height)
+        setBoardWidth(boundingRect.width)
       }
     })
     if (boardRef.current) {
@@ -383,6 +444,7 @@ export function ChessBoard() {
       if (boardRef.current) {
         const boundingRect = boardRef.current.getBoundingClientRect()
         rect.current = getRect(boundingRect.top, boundingRect.left, boundingRect.width, boundingRect.height)
+        setBoardWidth(boundingRect.width)
       }
     })
 
@@ -391,23 +453,28 @@ export function ChessBoard() {
       if (boardRef.current) {
         const boundingRect = boardRef.current.getBoundingClientRect()
         rect.current = getRect(boundingRect.top, boundingRect.left, boundingRect.width, boundingRect.height)
+        setBoardWidth(boundingRect.width)
       }
     })
 
     return () => {
       resizeObserver.disconnect()
+
       window.removeEventListener('scroll', () => {
         console.log("RESIZE")
         if (boardRef.current) {
           const boundingRect = boardRef.current.getBoundingClientRect()
           rect.current = getRect(boundingRect.top, boundingRect.left, boundingRect.width, boundingRect.height)
+          setBoardWidth(boundingRect.width)
         }
       })
+
       window.removeEventListener('resize', () => {
         console.log("RESIZE")
         if (boardRef.current) {
           const boundingRect = boardRef.current.getBoundingClientRect()
           rect.current = getRect(boundingRect.top, boundingRect.left, boundingRect.width, boundingRect.height)
+          setBoardWidth(boundingRect.width)
         }
       })
 
@@ -425,33 +492,45 @@ export function ChessBoard() {
       setPromotionSquare
     )
   }, [game.matchData.activeMove])
+
+  const squareWidth = boardWidth / 8
   
   return (
-    <div className='chessboard' onClick={(event) => clickHandler(
-      event,
-      game,
-      rect,
-      promotionActive,
-      promotionSquare,
-      moves,
-      setMoves,
-      captures,
-      setCaptures,
-      selectedPiece,
-      setSelectedPiece,
-      promotionNextMove,
-      setPromotionNextMove,
-      setPromotionActive,
-      setPromotionSquare,
-      waiting,
-      setWaiting
-    )} ref={boardRef}>
-      <LastMoveComponent flip={game.flip}/>
-      <PiecesComponent flip={game.flip}/>
-      <MovesComponent moves={moves} flip={game.flip}/>
-      <CapturesComponent captures={captures} flip={game.flip}/>
-      <PromotionComponent promotionSquare={promotionSquare} promotionActive={promotionActive} flip={game.flip}/>
-      <GameOverComponent />
+    <div className="chessboard-container" style={{width: `${boardWidth}px`, height: `${boardWidth}px`}} ref={boardRef}>
+      <div 
+        className='chessboard' 
+        style={{
+          width: `${boardWidth}px`,
+          height: `${boardWidth}px`,
+          backgroundSize: `${boardWidth}px`,
+        }} 
+        onClick={(event) => clickHandler(
+          event,
+          game,
+          rect,
+          promotionActive,
+          promotionSquare,
+          moves,
+          setMoves,
+          captures,
+          setCaptures,
+          selectedPiece,
+          setSelectedPiece,
+          promotionNextMove,
+          setPromotionNextMove,
+          setPromotionActive,
+          setPromotionSquare,
+          waiting,
+          setWaiting
+        )}
+      >
+        <LastMoveComponent flip={game.flip} squareWidth={squareWidth}/>
+        <PiecesComponent flip={game.flip} squareWidth={squareWidth}/>
+        <MovesComponent moves={moves} flip={game.flip} squareWidth={squareWidth}/>
+        <CapturesComponent captures={captures} flip={game.flip} squareWidth={squareWidth}/>
+        <PromotionComponent promotionSquare={promotionSquare} promotionActive={promotionActive} flip={game.flip} squareWidth={squareWidth}/>
+        <GameOverComponent squareWidth={squareWidth}/>
+      </div>
     </div>
   )
 }
